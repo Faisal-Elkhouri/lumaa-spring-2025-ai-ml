b21f616743a48eb3df8a2aa0b9d8b61be72c1b15
Faisal-Elkhouri <felkhouri@ucsd.edu>
2025-02-21 20:37:01 -0800

    yeah

--- recommender.py
+++ recommender.py
@@ -1,13 +1,25 @@
 import numpy as np
 import pandas as pd
+import re
 
 # Turns any string (in this case user input) into an array of lowercase words
-def CleanText(userInput: str) -> str:
-    # Coverts any Uppercase letters to Lowercase
+def CleanText(userInput: str) -> list[str]:
+    # Coverts any uppercase letters to lowercase
     lowercaseInput = userInput.lower()
-    # Removes all text that is not a Lowercase letter or a whitespace
-    letterOnlyInput = lowercaseInput.replace(r"[^a-z\s]"," ")
-    # 
-    cleanInput = letterOnlyInput.split(r"\s")
+    # Removes all text that is not a lowercase letter or a whitespace
+    letterOnlyInput = re.sub(r"[^a-z ]"," ", lowercaseInput)
+    # Separates all words into a list of strings
+    seperatedInput = letterOnlyInput.split(" ")
+    # Removes any empty elements
+    cleanInput = [word for word in seperatedInput if word != ""]
+    return cleanInput
+
+# Remove common words
+def RemoveCommonWords(wordList: list[str]) -> list[str]:
+    
+
+anArray = CleanText("hEllo.worLD9999") 
+for text in anArray:
+    print(text)
 
     
